ef7bd193b86267ea28adad95db3bc02af41c92ab
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2026-01-02 22:19:36 +0300

    numbers

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function myFunction(a, b){
 
 results = myFunction(3, 5)
 
-console.log(myFunction.toString())
+
+
+// console.log((12+32).toString())
+
+//number()
+
+let nums = "20"
+// console.log(Number(Math.PI))
+
+//dates
+
+let date = new Date()
+
+console.log(date.toString())
